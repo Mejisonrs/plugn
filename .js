bc9c8a9b3881813ec94883647ac31db45e4e1bb9
Mejisonrs <130410762+Mejisonrs@users.noreply.github.com>
2023-04-13 12:14:36 -0400

    Update .js

--- .js
+++ .js
@@ -1,15 +1,12 @@
 const menu = document.querySelector(".aside");
 const buttons = document.querySelectorAll(".menu");
 const overlay = document.querySelector(".overlay");
-/*const cliner = document.querySelector('input[name="q"]');*/
 
 buttons.forEach(button => button.addEventListener("click", toggleMenu));
 overlay.addEventListener("click", closeMenu);
 document.addEventListener("keydown", event => {
   if (event.ctrlKey && event.key === "z") {
     toggleMenu();
-    /*cliner.focus();
-    cliner.value = "";*/
   }
 });
 
