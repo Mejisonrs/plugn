865238bf3a0338994e7cf368ca6097288bcde02b
Home <130410762+Mejisonrs@users.noreply.github.com>
2023-04-14 16:02:58 -0400

    js

---
+++ main.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector(".aside");
+const buttons = document.querySelectorAll(".menu");
+const overlay = document.querySelector(".overlay");
+
+buttons.forEach(button => button.addEventListener("click", toggleMenu));
+overlay.addEventListener("click", closeMenu);
+document.addEventListener("keydown", event => {
+  if (event.ctrlKey && event.key === "z") {
+    toggleMenu();
+  }
+});
+
+function toggleMenu() {
+  overlay.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+
+function closeMenu(event) {
+  overlay.classList.remove("active");
+  menu.classList.remove("active");
+}
